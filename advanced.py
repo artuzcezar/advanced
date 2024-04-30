9054b27a3396d1d31bffc1fe0c1f9a0c3333fa23
Artuz Cezar <157129485+artuzcezar@users.noreply.github.com>
2024-04-30 14:30:14 +0800

    advanced.py

--- advanced.py
+++ advanced.py
@@ -1,4 +1,4 @@
-def check_relationship(subject_user, object_user, relationships):
+def relationship_status(subject_user, object_user, relationships):
     if object_user in relationships[subject_user]["following"]:
         if subject_user in relationships[object_user]["following"]:
             return "friends"
@@ -9,26 +9,30 @@ def check_relationship(subject_user, object_user, relationships):
     else:
         return "no relationship"
 
-def check_winner(game_board):
-    board_size = len(game_board)
-    for i in range(board_size):
-        if all(game_board[i][0] == game_board[i][j] != '' for j in range(board_size)):
-            return game_board[i][0]
-        if all(game_board[0][i] == game_board[j][i] != '' for j in range(board_size)):
-            return game_board[0][i]
-    if all(game_board[0][0] == game_board[i][i] != '' for i in range(board_size)):
+def tic_tac_toe(game_board):
+    n = len(game_board)
+    for row in game_board:
+        if all(cell == row[0] and cell != '' for cell in row):
+            return row[0]
+    for col_index in range(n):
+        if all(game_board[row_index][col_index] == game_board[0][col_index] and game_board[row_index][col_index] != '' for row_index in range(n)):
+            return game_board[0][col_index]
+    if all(game_board[index][index] == game_board[0][0] and game_board[index][index] != '' for index in range(n)):
         return game_board[0][0]
-    if all(game_board[0][board_size-1] == game_board[i][board_size-1-i] != '' for i in range(board_size)):
-        return game_board[0][board_size-1]
+    if all(game_board[index][n - 1 - index] == game_board[0][n - 1] and game_board[index][n - 1 - index] != '' for index in range(n)):
+        return game_board[0][n - 1]
     return "NO WINNER"
 
-def calculate_eta(start_stop, end_stop, travel_routes):
-    travel_time = 0
+
+def eta(start_stop, end_stop, stop_times):
     current_location = start_stop
+    total_time = 0
     while current_location != end_stop:
-        for (start, destination), travel_details in travel_routes.items():
-            if start == current_location:
-                travel_time += travel_details['travel_time_mins']
+        for (origin, destination), travel_info in stop_times.items():
+            if origin == current_location:
+                total_time += travel_info['travel_time_mins']
                 current_location = destination
                 break
-    return travel_time
+    return total_time
+
+
